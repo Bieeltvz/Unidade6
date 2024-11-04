92931371775adcbd550a757b8e8693b5bc56f7b4
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2024-11-04 19:57:41 -0300

    Add files via upload

--- Uni6Exe03.java
+++ Uni6Exe03.java
@@ -1,6 +1,50 @@
 package Unidade6;
-
+import java.util.Scanner;
 
 public class Uni6Exe03 {
-    
+
+    public Uni6Exe03(){
+        double [] vetor = lerValores(12);
+        mostrarVetor(vetor);
+        alterarValores(vetor);
+        mostrarVetor(vetor);
+    }
+
+
+    private double [] lerValores(int tamanho){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("*** LEITURA DO VETOR ***");
+        double [] vetor = new double[tamanho];
+        for(int i = 0; i < vetor.length; i++){
+            System.out.printf("Posição %d ", i);
+            vetor [i] = teclado.nextDouble();
+        }
+        return vetor;
+    }
+
+    private void alterarValores(double [] vetor){
+        for(int i = 0; i < vetor.length; i++){
+            if(i % 2 == 0){
+                vetor[i] *= 1.02;
+            } else {
+                vetor[i] *= 1.05;
+            }
+        }
+    }
+
+    private void mostrarVetor(double [] vetor){
+       System.out.println("[ ");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.printf("%.2f ", vetor[i]);
+        }
+        System.out.println(" ] ");
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new Uni6Exe03();
+    }
 }
